8056ea56c93df99e0d5d620375e3255ba200c3d0
Juan Pablo <uncorreodeinternet@gmail.com>
2020-10-24 17:27:35 -0500

    [#3] Generate n amount of random numbers

--- number_generator.py
+++ number_generator.py
@@ -9,3 +9,8 @@ class NumberGenerator:
             return random_number
         except ValueError:
             print(f"Values {lower_range} and {upper_range} are invalid")
+
+    def generate_n_between_range(self, amount: int, lower_range: int, upper_range: int):
+        return [
+            self.generate_random_number(lower_range, upper_range) for _ in range(amount)
+        ]
