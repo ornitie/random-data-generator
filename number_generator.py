af7dd8d55a30f8fde89b13d66691d39e48bd0577
Juan Pablo <uncorreodeinternet@gmail.com>
2021-01-10 19:09:54 -0500

    [#9] Added unique number and fixed sorted number generators

--- number_generator.py
+++ number_generator.py
@@ -15,18 +15,31 @@ class NumberGenerator:
             self.generate_random_number(lower_range, upper_range) for _ in range(amount)
         ]
 
-    def generate_sorted_random_numbers(
+    def generate_n_unique_numbers(
         self, amount: int, lower_range: int, upper_range: int
+    ):
+        visited = {}
+        counter = 0
+
+        while counter < amount:
+            num = self.generate_random_number(lower_range, upper_range)
+            if visited.get(num) != None:
+                continue
+            visited[num] = 1
+            counter += 1
+
+        return sorted(visited.keys())
+
+    def generate_sorted_random_numbers(
+        self, amount: int, lower_range: int, upper_range: int, unique: bool = True
     ):
         if amount > (upper_range - lower_range):
             raise ValueError(
                 f"Amount {amount} cannot be bigger than the range of numbers"
             )
 
-        floor = lower_range
-        sorted_nums = []
-        for i in range(amount, 0, -1):
-            floor = self.generate_random_number(floor + 1, upper_range - i)
-            sorted_nums.append(floor)
-
-        return sorted_nums
+        return (
+            sorted(self.generate_n_unique_numbers(amount, lower_range, upper_range))
+            if unique
+            else sorted(self.generate_n_between_range(amount, lower_range, upper_range))
+        )
